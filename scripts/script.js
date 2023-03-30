ccb127900c914fe08d05c8cef35345a6ea0d8b6d
Soojeong Park <soojeong.park.ca@gmail.com>
2023-03-30 16:47:41 -0700

    complete functions for form step1 and step2

--- scripts/script.js
+++ scripts/script.js
@@ -2,172 +2,372 @@
 
 /********************************************/
 /********* Page Elements *********/
-const personalInfoForm = document.querySelector(".form-personal-info");
-
-const selectPlanForm = document.querySelector(".form-select-plan");
-const planPeriodSelector = document.querySelector(".plan-period");
-
-const addOnsForm = document.querySelector(".form-add-ons");
-
-const summary = document.querySelector(".summary");
-const totalCost = document.querySelector(".total-cost");
-
-const confirmed = document.querySelector(".confirmed");
+const personalInfo = document.getElementById("personal-info");
+const selectPlan = document.getElementById("select-plan");
+const addOns = document.getElementById("add-ons");
+const summary = document.getElementById("summary");
+const confirmed = document.getElementById("confirmed");
 
 /********************************************/
 /********* General *********/
 const personalInfoData = [];
+/*
+  example:
+  {
+    id: crypto.randomUUID(),
+    userName: "Jane Doe",
+    userEmail: "jane@doe.com",
+    userPhone: "123 123 1234",
+    plan: {planName: "arcade-monthly"},
+    addOns: {onlineService: true, largerStorage: false, customizableProfile: true}
+  }
+*/
+let currentUserInfo = {
+  id: crypto.randomUUID(),
+  userName: "",
+  userEmail: "",
+  userPhone: "",
+  plan: "arcade-monthly",
+  addOns: {
+    onlineService: true,
+    largerStorage: true,
+    customizableProfile: false,
+  },
+};
 
+const allNavBtns = document.querySelectorAll(".nav-btn");
+const navBtn1 = document.querySelector(".nav-btn-1");
+const navBtn2 = document.querySelector(".nav-btn-2");
+const navBtn3 = document.querySelector(".nav-btn-3");
+const navBtn4 = document.querySelector(".nav-btn-4");
+
+const btnNext = document.querySelector(".btn-next");
+const btnBack = document.querySelector(".btn-back");
+
+/********************************************/
+/********* Personal Info Page *********/
+// function personalInfoPage() {}
+// VARIABLES
 const allInputs = document.querySelectorAll(".input");
 const userName = document.getElementById("input-name");
 const userEmail = document.getElementById("input-email");
 const userPhone = document.getElementById("input-phone");
 
-const btnNext = document.querySelector(".btn-next");
-const btnBack = document.querySelector(".btn-back");
+// FUNCTIONS
+/*
+  // Show corresponding element
+  personalInfo.classList.remove("hidden");
+  selectPlan.classList.add("hidden");
+  addOns.classList.add("hidden");
+  summary.classList.add("hidden");
 
-// NAV
-const allNavBtns = document.querySelectorAll(".nav-btn");
+  // Hide Go Back Btn
+  btnBack.classList.add("hidden-btn");
+  */
 
-function handleNavBtn(e) {
-  allNavBtns.forEach(btn => btn.classList.remove("current-page"));
-  e.target.classList.add("current-page");
+// Saving user data
+function saveUserInfo() {
+  currentUserInfo.userName = userName.value;
+  currentUserInfo.userEmail = userEmail.value;
+  currentUserInfo.userPhone = userPhone.value;
+
+  console.log("Personal Info Page: ", currentUserInfo);
 }
 
-allNavBtns.forEach(btn => {
-  btn.addEventListener("click", handleNavBtn);
+// Update input value attribute so the values stay in place
+function updateInputValue() {
+  userName.setAttribute("value", userName.value);
+  userEmail.setAttribute("value", userEmail.value);
+  userPhone.setAttribute("value", userPhone.value);
+}
+
+// Input Validation
+// 1 - Live Empty Input Check
+function liveEmptyInputCheck(e) {
+  const inputHint = e.target
+    .closest(".form-item")
+    .querySelector(".input-label")
+    .querySelector(".input-hint");
+
+  if (e.target.value.trim().length > 0) {
+    e.target.classList.remove("red-border");
+    inputHint.textContent = "";
+  } else {
+    e.target.classList.add("red-border");
+    inputHint.textContent = "This field is required";
+  }
+}
+
+allInputs.forEach(input => {
+  input.addEventListener("input", updateInputValue);
+  input.addEventListener("input", liveEmptyInputCheck);
 });
+allInputs.forEach(input =>
+  input.addEventListener("focusout", liveEmptyInputCheck)
+);
+
+// 2 - Form Validation Check (for after btn click)
+function validateForm() {
+  const nameRegex = /^[a-zA-Z ]{2,30}$/; // Only letters and spaces, 2-30 characters
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const phoneRegex =
+    /^(\+?\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$|^(\+?\d{1,2}\s)?\d{3}[\s-]?\d{3}[\s-]?\d{4}$/;
+  const userNameVal = userName.value.trim();
+  const userEmailVal = userEmail.value.trim();
+  const userPhoneVal = userPhone.value.trim();
+
+  const nameHint = document.querySelector(".name-hint");
+  const emailHint = document.querySelector(".email-hint");
+  const phoneHint = document.querySelector(".phone-hint");
+
+  function emptyInput(input, inputHint) {
+    input.classList.add("red-border");
+    inputHint.textContent = "This field is required";
+  }
+
+  function invalidInput(input, inputHint) {
+    input.classList.add("red-border");
+    inputHint.textContent = `Invalid ${input.name}`;
+  }
+
+  function validInput(input, inputHint) {
+    input.classList.remove("red-border");
+    inputHint.textContent = "";
+  }
+
+  // Display hint, + border
+  if (userNameVal.length === 0) {
+    emptyInput(userName, nameHint);
+  } else if (!nameRegex.test(userNameVal)) {
+    invalidInput(userName, nameHint);
+  } else if (nameRegex.test(userNameVal)) {
+    validInput(userName, nameHint);
+  }
+
+  if (userEmailVal.length === 0) {
+    emptyInput(userEmail, emailHint);
+  } else if (!emailRegex.test(userEmailVal)) {
+    invalidInput(userEmail, emailHint);
+  } else if (emailRegex.test(userEmailVal)) {
+    validInput(userEmail, emailHint);
+  }
+
+  if (userPhoneVal.length === 0) {
+    emptyInput(userPhone, phoneHint);
+  } else if (!phoneRegex.test(userPhoneVal)) {
+    invalidInput(userPhone, phoneHint);
+  } else if (phoneRegex.test(userPhoneVal)) {
+    validInput(userPhone, phoneHint);
+  }
+
+  if (
+    nameRegex.test(userNameVal) &&
+    emailRegex.test(userEmailVal) &&
+    phoneRegex.test(userPhoneVal)
+  ) {
+    // Save form info
+    saveUserInfo();
+
+    // Nav button style change
+    navBtn1.classList.remove("current-page");
+    navBtn3.classList.remove("current-page");
+    navBtn4.classList.remove("current-page");
+    navBtn2.classList.add("current-page");
+
+    // Showing the corresponding page
+    selectPlan.classList.remove("hidden");
+
+    personalInfo.classList.add("hidden");
+    addOns.classList.add("hidden");
+    summary.classList.add("hidden");
+
+    btnBack.classList.remove("hidden-btn");
+  }
+}
+
+// Click the Next Button
+btnNext.addEventListener("click", validateForm);
 
 /********************************************/
-/********* Personal Info Page *********/
-function personalInfoPage() {
-  // FUNCTIONS
-  // Saving user data
-  function saveUserInfo() {
-    personalInfoData.push({
-      id: crypto.randomUUID(),
-      userName: userName.value,
-      userEmail: userEmail.value,
-      userPhone: userPhone.value,
-    });
-    console.log("script: ", personalInfoData);
-  }
+/********* Select Plan Page *********/
+function selectPlanPage() {
+  // VARIABLES
+  const allPlanItems = document.querySelectorAll(".form-item-plan");
+  const monthlyPlans = document.querySelector(".form-monthly-plans");
+  const yearlyPlans = document.querySelector(".form-yearly-plans");
 
-  // Update input value attribute
-  function updateInputValue() {
-    userName.setAttribute("value", userName.value);
-    userEmail.setAttribute("value", userEmail.value);
-    userPhone.setAttribute("value", userPhone.value);
-  }
+  const btnMonthly = document.querySelector(".btn-monthly");
+  const btnYearly = document.querySelector(".btn-yearly");
+  const btnTogglePlans = document.querySelector(".btn-toggle");
 
-  // Input Validation
-  // 1 - Live Empty Input Check
-  function liveEmptyInputCheck(e) {
-    const inputHint = e.target
-      .closest(".form-item")
-      .querySelector(".input-label")
-      .querySelector(".input-hint");
+  // Default plan selected - monthly arcade plan
+  let selectedPlan = document.querySelector(`.${currentUserInfo.plan}`);
+  let selectedPlanInput = selectedPlan.querySelector("input");
 
-    if (e.target.value.trim().length > 0) {
-      e.target.classList.remove("red-border");
-      inputHint.textContent = "";
-    } else {
-      e.target.classList.add("red-border");
-      inputHint.textContent = "This field is required";
-    }
-  }
-
-  allInputs.forEach(input => {
-    input.addEventListener("input", updateInputValue);
-    input.addEventListener("input", liveEmptyInputCheck);
+  allPlanItems.forEach(item => {
+    item.classList.remove("checked");
+    item.querySelector("input").setAttribute("checked", false);
   });
-  allInputs.forEach(input =>
-    input.addEventListener("focusout", liveEmptyInputCheck)
-  );
+  selectedPlan.classList.add("checked");
+  selectedPlanInput.setAttribute("checked", true);
+  currentUserInfo.plan = selectedPlanInput.value;
 
-  // 2 - Form Validation Check (for after btn click)
-  function validateForm() {
-    const nameRegex = /^[a-zA-Z ]{2,30}$/; // Only letters and spaces, 2-30 characters
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const phoneRegex =
-      /^(\+?\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$|^(\+?\d{1,2}\s)?\d{3}[\s-]?\d{3}[\s-]?\d{4}$/;
-    const userNameVal = userName.value.trim();
-    const userEmailVal = userEmail.value.trim();
-    const userPhoneVal = userPhone.value.trim();
+  console.log(currentUserInfo);
 
-    const nameHint = document.querySelector(".name-hint");
-    const emailHint = document.querySelector(".email-hint");
-    const phoneHint = document.querySelector(".phone-hint");
+  // FUNCTIONS
+  // // Nav button style change
+  // navBtn1.classList.remove("current-page");
+  // navBtn3.classList.remove("current-page");
+  // navBtn4.classList.remove("current-page");
+  // navBtn2.classList.add("current-page");
 
-    function emptyInput(input, inputHint) {
-      input.classList.add("red-border");
-      inputHint.textContent = "This field is required";
-    }
+  // // Showing the corresponding page
+  // selectPlan.classList.remove("hidden");
 
-    function invalidInput(input, inputHint) {
-      input.classList.add("red-border");
-      inputHint.textContent = `Invalid ${input.name}`;
-    }
+  // personalInfo.classList.add("hidden");
+  // addOns.classList.add("hidden");
+  // summary.classList.add("hidden");
 
-    function validInput(input, inputHint) {
-      input.classList.remove("red-border");
-      inputHint.textContent = "";
-    }
+  // btnBack.classList.remove("hidden-btn");
 
-    if (userNameVal.length === 0) {
-      emptyInput(userName, nameHint);
-    } else if (!nameRegex.test(userNameVal)) {
-      invalidInput(userName, nameHint);
-    } else if (nameRegex.test(userNameVal)) {
-      validInput(userName, nameHint);
-    }
+  // Default plan selected - monthly arcade plan
+  /*
+  let selectedPlan = monthlyPlans.querySelectorAll(".form-item-plan")[0];
+  selectedPlan.classList.add("checked");
+  let selectedPlanInput = selectedPlan.querySelector("input");
+  selectedPlanInput.setAttribute("checked", true);
+  console.log(selectedPlan, selectedPlanInput);
+  currentUserInfo.plan = { planName: selectedPlanInput.value };
+  console.log(currentUserInfo);
+  console.log(personalInfoData);
+  */
 
-    if (userEmailVal.length === 0) {
-      emptyInput(userEmail, emailHint);
-    } else if (!emailRegex.test(userEmailVal)) {
-      invalidInput(userEmail, emailHint);
-    } else if (emailRegex.test(userEmailVal)) {
-      validInput(userEmail, emailHint);
-    }
+  // 1 - clicking / unclicking plan item
+  // add/remove styles + add/remove checked attribute from radio inputs
+  function choosePlan() {
+    allPlanItems.forEach(item => {
+      item.classList.remove("checked");
+      item.querySelector("input").setAttribute("checked", false);
+    });
 
-    if (userPhoneVal.length === 0) {
-      emptyInput(userPhone, phoneHint);
-    } else if (!phoneRegex.test(userPhoneVal)) {
-      invalidInput(userPhone, phoneHint);
-    } else if (phoneRegex.test(userPhoneVal)) {
-      validInput(userPhone, phoneHint);
-    }
+    // update selected plan
+    selectedPlan = this;
+    selectedPlanInput = this.querySelector("input");
 
+    this.classList.add("checked");
+    selectedPlanInput.setAttribute("checked", true);
+
+    // find current user data and update the plan
+    currentUserInfo.plan = selectedPlanInput.value;
+    console.log(currentUserInfo);
+  }
+
+  allPlanItems.forEach(item => item.addEventListener("click", choosePlan));
+
+  // 3 - Toggle Display - Monthly or Yearly
+  // display Monthly plans
+  function displayMonthlyPlans() {
+    // buttons appearance
+    btnYearly.classList.remove("chosen-period");
+    btnMonthly.classList.add("chosen-period");
+
+    btnTogglePlans
+      .querySelector("img")
+      .setAttribute("src", "../assets/images/icon-toggle-left.svg");
+
+    // changing plan
+    //- unchoose all yearly plans
+    yearlyPlans.querySelectorAll(".form-item").forEach(item => {
+      item.classList.remove("checked");
+      item.querySelector("input").setAttribute("checked", false);
+    });
+    currentUserInfo.plan = "";
+
+    //- choose first plan from monthly plans
+    monthlyPlans
+      .querySelectorAll(".form-item-plan")[0]
+      .querySelector("input")
+      .setAttribute("checked", true);
+    monthlyPlans
+      .querySelectorAll(".form-item-plan")[0]
+      .classList.add("checked");
+
+    // find current user data and update the plan
+    currentUserInfo.plan = monthlyPlans
+      .querySelectorAll(".form-item-plan")[0]
+      .querySelector("input").value;
+    console.log(currentUserInfo);
+
+    // hide yearly plans and show monthly plans
+    yearlyPlans.classList.add("hidden");
+    monthlyPlans.classList.remove("hidden");
+  }
+  // when Monthly button clicked
+  btnMonthly.addEventListener("click", displayMonthlyPlans);
+
+  // display Yearly plans
+  function displayYearlyPlans() {
+    // buttons appearance
+    btnMonthly.classList.remove("chosen-period");
+    btnYearly.classList.add("chosen-period");
+
+    btnTogglePlans
+      .querySelector("img")
+      .setAttribute("src", "../assets/images/icon-toggle-right.svg");
+
+    // changing plan
+    //- unchoose all yearly plans
+    monthlyPlans.querySelectorAll(".form-item-plan").forEach(item => {
+      item.classList.remove("checked");
+      item.querySelector("input").setAttribute("checked", false);
+    });
+
+    //- choose first plan from yearly plans
+    yearlyPlans
+      .querySelectorAll(".form-item-plan")[0]
+      .querySelector("input")
+      .setAttribute("checked", true);
+    yearlyPlans.querySelectorAll(".form-item-plan")[0].classList.add("checked");
+
+    // find current user data and update the plan
+    currentUserInfo.plan = yearlyPlans
+      .querySelectorAll(".form-item")[0]
+      .querySelector("input").value;
+    console.log(currentUserInfo);
+
+    // hide monthly plans and show yearly plans
+    monthlyPlans.classList.add("hidden");
+    yearlyPlans.classList.remove("hidden");
+  }
+  // when Yearly button clicked
+  btnYearly.addEventListener("click", displayYearlyPlans);
+
+  // toggle plans
+  function togglePlans() {
     if (
-      nameRegex.test(userNameVal) &&
-      emailRegex.test(userEmailVal) &&
-      phoneRegex.test(userPhoneVal)
+      btnTogglePlans.querySelector("img").getAttribute("src") ===
+      "../assets/images/icon-toggle-left.svg"
     ) {
-      // Save form info
-      saveUserInfo();
-
-      /*
-      // Redirect to next html page
-      const nextPageUrl = htmlPages.find(
-        page => page.pageNum === currentPageNum + 1
-      ).url;
-
-      window.location.replace(nextPageUrl);
-      */
-
-      // Hide Personal Info
-      personalInfoForm.classList.add("hidden");
-      // Show Select Plan + Go Back btn
-      selectPlanForm.classList.remove("hidden");
-      planPeriodSelector.classList.remove("hidden");
-      btnBack.classList.remove("hidden-btn");
+      displayYearlyPlans();
+    } else if (
+      btnTogglePlans.querySelector("img").getAttribute("src") ===
+      "../assets/images/icon-toggle-right.svg"
+    ) {
+      displayMonthlyPlans();
     }
   }
-  btnNext.addEventListener("click", validateForm);
+
+  // when toggle icon clicked
+  btnTogglePlans.addEventListener("click", togglePlans);
+
+  // Go Back Button
+  btnBack.addEventListener("click", function () {
+    // Hide Select Plan + Go Back btn
+    selectPlan.classList.add("hidden");
+    btnBack.classList.add("hidden-btn");
+    // Show Personal Info
+    personalInfo.classList.remove("hidden");
+  });
 }
 
-/********* Select Plan Page *********/
-function selectPlanPage() {}
-
-// CALL FNS
-personalInfoPage();
+selectPlanPage();
